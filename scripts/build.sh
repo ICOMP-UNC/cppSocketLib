88b5262969388c598d04abefea7296c027e8c15a
manugcr <mgilcernich@gmail.com>
2024-04-23 12:31:01 -0300

    First cmake config version

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+# !bin/bash -e
+
+echo ""
+
+if [ -d "./build" ]; then
+    echo "  -> Removing old build directory..."
+    rm -rf ./build/*
+else
+    echo "  -> Creating build directory..."
+    mkdir build
+fi
+
+echo "  -> Building project"
+echo ""
+cd build && cmake -GNinja .. && ninja
+
+# echo "  -> Running project"
+# echo ""
+# ./FirstProjectInCMake
