7af2d120b27f3df1d031cc27e44f134be036dc37
w3rqil <leonel.mansilla@mi.unc.edu.ar>
2024-04-23 12:33:00 -0300

    feature update

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+
+# Volver al directorio original# !bin/bash -e
+
+echo ""
+
+if [ -d "./build" ]; then
+    echo "  -> Removing old build directory..."
+    rm -rf ./build/*
+else
+    echo "  -> Creating build directory..."
+    mkdir build
+fi
+
+echo "  -> Building project"
+echo ""
+cd build && cmake -GNinja -DRUN_TESTS=0 -DRUN_COVERAGE=0 .. && ninja
+
+echo " -> Running project"
+./FirstProjectInCMake
