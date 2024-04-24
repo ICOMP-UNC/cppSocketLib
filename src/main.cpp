f7e565f0dcb3a23dc4670ee0332719e8a250c7f0
Jolx77 <joaquin.otalora@mi.unc.edu.ar>
2024-04-24 16:22:35 -0300

    Corregidos cambios solicitados. Ahora compila y utiliza string en vez de char*

--- src/main.cpp
+++ src/main.cpp
@@ -1,133 +1,136 @@
 #include "libraryCpp.hpp"
 
-
-IConnection::IConnection(const std::string &address, const std::string &port, 
-                              bool isBlocking) : address_(address), m_port_(port), isBlocking_(isBlocking) {}
+IConnection::IConnection(const std::string &address, const std::string &port,
+                         bool isBlocking)
+    : address_(address), m_port_(port), isBlocking_(isBlocking) {}
 
 IConnection::~IConnection() {}
 
 int IConnection::getSocket() { return socket_fd_; }
 
-TCPv4Connection::TCPv4Connection(const std::string& address, const std::string& port, bool isBlocking) 
-  : IConnection(address, port, isBlocking) {
+TCPv4Connection::TCPv4Connection(const std::string &address,
+                                 const std::string &port, bool isBlocking)
+    : IConnection(address, port, isBlocking) {
   // Implementación del constructor aquí
 }
 
-
 bool TCPv4Connection::bind() {
-    // Implementación del método bind
-    return true;
+  // Implementación del método bind
+  return true;
 }
 
 int TCPv4Connection::connect() {
-    // Implementación del método connect
-    return 0;
+  // Implementación del método connect
+  return 0;
 }
 
 bool TCPv4Connection::send(const std::string &message) {
-    // Implementación del método send
-    return true;
+  // Implementación del método send
+  return true;
 }
 
 std::string TCPv4Connection::receive() {
-    // Implementación del método receive
-    return "";
+  // Implementación del método receive
+  return "";
 }
 
 bool TCPv4Connection::changeOptions() {
-    // Implementación del método changeOptions
-    return true;
+  // Implementación del método changeOptions
+  return true;
 }
 
-TCPv6Connection::TCPv6Connection(const std::string& address, const std::string& port, bool isBlocking) 
-  : IConnection(address, port, isBlocking) {
+TCPv6Connection::TCPv6Connection(const std::string &address,
+                                 const std::string &port, bool isBlocking)
+    : IConnection(address, port, isBlocking) {
   // Implementación del constructor aquí
 }
 
 bool TCPv6Connection::bind() {
-    // Implementación del método bind
-    return true;
+  // Implementación del método bind
+  return true;
 }
 
 int TCPv6Connection::connect() {
-    // Implementación del método connect
-    return 0;
+  // Implementación del método connect
+  return 0;
 }
 
 bool TCPv6Connection::send(const std::string &message) {
-    // Implementación del método send
-    return true;
+  // Implementación del método send
+  return true;
 }
 
 std::string TCPv6Connection::receive() {
-    // Implementación del método receive
-    return "";
+  // Implementación del método receive
+  return "";
 }
 
 bool TCPv6Connection::changeOptions() {
-    // Implementación del método changeOptions
-    return true;
+  // Implementación del método changeOptions
+  return true;
 }
 
-UDPConnection::UDPConnection(const std::string &address, const std::string &port, bool isBlocking) : IConnection(address, port, isBlocking) {
-    
-    struct addrinfo hints;
-    memset(&hints, 0, sizeof(hints));
+UDPConnection::UDPConnection(const std::string &address,
+                             const std::string &port, bool isBlocking)
+    : IConnection(address, port, isBlocking) {
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(hints));
 
-    bool isIPv6 = address.find(':') != std::string::npos;
-    if (isIPv6) {
-      hints.ai_family = AF_INET6;
-    } else {
-      hints.ai_family = AF_INET;
-    }
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_protocol = 0;
+  bool isIPv6 = address.find(':') != std::string::npos;
+  if (isIPv6) {
+    hints.ai_family = AF_INET6;
+  } else {
+    hints.ai_family = AF_INET;
+  }
+  hints.ai_socktype = SOCK_DGRAM;
+  hints.ai_protocol = 0;
 
-    if (address.empty()) {
-      hints.ai_flags = AI_PASSIVE;
-    }
+  if (address.empty()) {
+    hints.ai_flags = AI_PASSIVE;
+  }
 
-    addrinfo* raw_addrinfo = nullptr; // Raw pointer to store the result of getaddrinfo
-    int res = getaddrinfo(address.c_str(), port.c_str(), &hints, &raw_addrinfo);
-    if (res != 0) {
-      std::cerr << "Error getting address" << std::endl;
-      exit(1);
-    }
-    m_addrinfo = std::unique_ptr<addrinfo>(raw_addrinfo); // Wrap raw pointer in the smart pointer
+  addrinfo *raw_addrinfo =
+      nullptr;  // Raw pointer to store the result of getaddrinfo
+  int res = getaddrinfo(address.c_str(), port.c_str(), &hints, &raw_addrinfo);
+  if (res != 0) {
+    std::cerr << "Error getting address" << std::endl;
+    exit(1);
+  }
+  m_addrinfo = std::unique_ptr<addrinfo>(
+      raw_addrinfo);  // Wrap raw pointer in the smart pointer
 
-    m_socket = socket(m_addrinfo->ai_family, m_addrinfo->ai_socktype,
-                      m_addrinfo->ai_protocol);
-    
-    if (m_socket < 0) {
-      throw std::runtime_error("Error creating socket");
-    }
-    if (!isBlocking) {
-      int flags = fcntl(m_socket, F_GETFL, 0);
-      fcntl(m_socket, F_SETFL, flags | O_NONBLOCK);
-    }
+  m_socket = socket(m_addrinfo->ai_family, m_addrinfo->ai_socktype,
+                    m_addrinfo->ai_protocol);
+
+  if (m_socket < 0) {
+    throw std::runtime_error("Error creating socket");
+  }
+  if (!isBlocking) {
+    int flags = fcntl(m_socket, F_GETFL, 0);
+    fcntl(m_socket, F_SETFL, flags | O_NONBLOCK);
+  }
 }
 
-UDPConnection::~UDPConnection() { 
-}
+UDPConnection::~UDPConnection() {}
 
 bool UDPConnection::bind() {
-    // Bind socket to address
-    if (::bind(m_socket, m_addrinfo->ai_addr, m_addrinfo->ai_addrlen) < 0) {
-      throw std::runtime_error("Error binding socket to address: ");
-      return false;
-    }
+  // Bind socket to address
+  if (::bind(m_socket, m_addrinfo->ai_addr, m_addrinfo->ai_addrlen) < 0) {
+    throw std::runtime_error("Error binding socket to address: ");
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 int UDPConnection::connect() {
-    // Connect socket to address
-    if (::connect(m_socket, m_addrinfo->ai_addr, m_addrinfo->ai_addrlen) < 0) {
-      throw std::runtime_error("Error getting address");
-      return false;
-    }
+  // Connect socket to address
+  if (::connect(m_socket, m_addrinfo->ai_addr, m_addrinfo->ai_addrlen) < 0) {
+    throw std::runtime_error("Error getting address");
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 bool UDPConnection::send(const std::string &message) {
@@ -144,32 +147,31 @@ bool UDPConnection::send(const std::string &message) {
 }
 
 std::string UDPConnection::receive() {
-    //char recv_message[MESSAGE_SIZE];
-    //memset(recv_message, 0, MESSAGE_SIZE);
+  std::vector<char> recv_message(MESSAGE_LENGTH, 0);
+  socklen_t addr_len = sizeof(m_addrinfo);
 
-    string recv_message;
-    socklen_t addr_len = sizeof(m_addrinfo);
+  // int bytes_received = ::recv(m_socket, recv_message, MESSAGE_SIZE, 0);
+  int bytes_received =
+      ::recv(m_socket, recv_message.data(), recv_message.size(), 0);
 
-    //int bytes_received = ::recv(m_socket, recv_message, MESSAGE_SIZE, 0);
-    int bytes_received = ::recv(m_socket, recv_message, recv_message.size(), 0);
+  if (bytes_received < 0) {
+    std::cerr << "Error receiving data: " << strerror(errno) << std::endl;
+    exit(EXIT_FAILURE);
+  }
 
-    if (bytes_received < 0) {
-      std::cerr << "Error receiving data: " << strerror(errno) << std::endl;
-      exit(EXIT_FAILURE);
-    }
-
-    return recv_message;
+  return std::string(recv_message.begin(),
+                     recv_message.begin() + bytes_received);
 }
 
 bool UDPConnection::changeOptions() {
-    // Implementación del método changeOptions
-    return true;
+  // Implementación del método changeOptions
+  return true;
 }
 
-
 std::unique_ptr<IConnection> createConnection(const std::string &address,
-                              const std::string &port, bool isBlocking,
-                              int protocolMacro) {
+                                              const std::string &port,
+                                              bool isBlocking,
+                                              int protocolMacro) {
   Protocol protocol;
 
   // Determinar si la dirección es IPv4 o IPv6
@@ -203,40 +205,55 @@ std::unique_ptr<IConnection> createConnection(const std::string &address,
 }
 
 int main() {
-  std::shared_ptr<IConnection> con1 = std::make_shared<UDPConnection>("::1", "7658", true);
-  std::shared_ptr<IConnection> con2 = std::make_shared<UDPConnection>("::1", "7658", true);
-  std::shared_ptr<IConnection> con3 = std::make_shared<UDPConnection>("127.0.0.2", "7672", true);
-  std::shared_ptr<IConnection> con4 = std::make_shared<UDPConnection>("127.0.0.2", "7672", true);
+  std::shared_ptr<IConnection> con1 =
+      std::make_shared<UDPConnection>("::1", "7658", true);
+  std::shared_ptr<IConnection> con2 =
+      std::make_shared<UDPConnection>("::1", "7658", true);
+  std::shared_ptr<IConnection> con3 =
+      std::make_shared<UDPConnection>("127.0.0.2", "7672", true);
+  std::shared_ptr<IConnection> con4 =
+      std::make_shared<UDPConnection>("127.0.0.2", "7672", true);
 
   std::jthread thread1([&con1]() {
     try {
       con1->bind();
       std::string message = con1->receive();
       std::cout << "Received message: " << message << std::endl;
-    } catch (const excepti&) {
-
+    } catch (const std::exception &e) {
+      std::cerr << e.what() << std::endl;
     }
 
     // Process the received message from con1
   });
 
   std::jthread thread2([&con2]() {
-    con2->connect();
-    con2->send("Hola mundo! IPv6");
-    // Process the received message from con2
+    try {
+      con2->connect();
+      con2->send("Hola mundo! IPv6");
+    } catch (const std::exception &e) {
+      std::cerr << e.what() << std::endl;
+    }
   });
 
   std::jthread thread4([&con4]() {
-    con4->connect();
-    con4->send("Hola mundo! IPv4");
-    // Process the received message from con2
+    try {
+      con4->connect();
+      con4->send("Hola mundo! IPv4");
+      // Process the received message from con2
+    } catch (const std::exception &e) {
+      std::cerr << e.what() << std::endl;
+    }
   });
 
   std::jthread thread3([&con3]() {
-    con3->bind();
-    std::string message = con3->receive();
-    std::cout << "Received message: " << message << std::endl;
-    // Process the received message from con1
+    try {
+      con3->bind();
+      std::string message = con3->receive();
+      std::cout << "Received message: " << message << std::endl;
+      // Process the received message from con1
+    } catch (const std::exception &e) {
+      std::cerr << e.what() << std::endl;
+    }
   });
 
   // Wait for the threads to finish
@@ -244,8 +261,6 @@ int main() {
   thread4.join();
   thread1.join();
   thread2.join();
- 
 
   return 0;
 }
-
