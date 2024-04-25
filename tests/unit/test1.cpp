0f0ac6cd0d71b461bf35b7b813c976a78422b825
yocamilo <camilo.gorriti@mi.unc.edu.ar>
2024-04-24 21:18:14 -0300

    [Tests]
    Added new tests for TCPv6

--- tests/unit/test1.cpp
+++ tests/unit/test1.cpp
@@ -7,6 +7,34 @@ TEST(TCPConnectionTestIPv4, BindSuccess) {
     
 }
 
+// Test to verify TCPv6Connection initialization is working
+TEST(TCPv6ConnectionTest, BindSuccess) {
+    TCPv6Connection conn("::1", "8080", false);
+
+    EXPECT_NO_THROW(conn.bind());
+}
+
+// Testo to verify the conection client-server is working
+TEST(TCPv6ConnectionTest, ConnectSuccess) {
+    TCPv6Connection server("::1", "8080", false);
+    TCPv6Connection client("::1", "8080", false);
+
+    server.bind();
+
+    EXPECT_NO_THROW(client.connect());
+}
+
+// Test to verify send the message correctly
+TEST(TCPv6ConnectionTest, SendMessage) {
+    TCPv6Connection server("::1", "8080", false);
+    TCPv6Connection client("::1", "8080", false);
+
+    server.bind();
+
+    client.connect();
+
+    EXPECT_NO_THROW(client.send("Hello, world!"));
+}
 
 
 int main(int argc, char** argv)
